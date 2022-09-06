c7651b548fcc9d785a59c95b76286d3ad0fc75d5
Raphael Jube <raphaeljube@ymail.com>
2022-09-06 14:24:51 +0200

    Initial commit for exercise 01-02

--- 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
+++ 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
@@ -1,6 +1,18 @@
 function insertDash(word) {
   // TODO: implement the method and return word with dashes
-  return null;
+  const vowels = ["a", "e", "i", "o", "u", "y", " "];
+  if (word.length === 0) {
+    return word;
+  }
+  let output = word[0];
+  for (let i = 1; i < word.length; i += 1) {
+    if (!vowels.includes(word[i].toLowerCase()) && !vowels.includes(word[i - 1].toLowerCase())) {
+      output += `-${word[i]}`;
+    } else {
+      output += word[i];
+    }
+  }
+  return output;
 }
 
 module.exports = insertDash;
